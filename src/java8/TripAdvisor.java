c230f470a361f638c5d47fb4b9c39905e3f0ac97
Son <namson0482@gmail.com>
2022-02-12 13:31:55 +0700

    change source of TripAdvisor.java

--- src/java8/TripAdvisor.java
+++ src/java8/TripAdvisor.java
@@ -4,5 +4,8 @@ public class TripAdvisor {
 
     public static void main(String[] args) {
         System.out.println("Vu Nam Son");
+        System.out.println("Vu Nam Son 111");
+        System.out.println("Vu Nam Son 333");
+        System.out.println("Vu Nam Son 222");
     }
 }
